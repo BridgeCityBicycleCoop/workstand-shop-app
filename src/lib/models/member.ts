1736e7e0cddb027b64a690f4d958afbd22b8c5d7
mitch <mitch@mukungfu.com>
2024-05-16 15:38:50 -0400

    fix types on member schema

--- src/lib/models/member.ts
+++ src/lib/models/member.ts
@@ -2,43 +2,48 @@ import z from 'zod';
 
 export const Member = z.object({
 	id: z.number(),
+	user_id: z.string(),
 	phone: z.string(),
 	email: z.string(),
 	email_consent: z.boolean(),
 	name: z.string(),
 	preferred_name: z.string(),
-	date_of_birth: z.date(),
+	date_of_birth: z.string(),
 	guardian_name: z.string(),
 	is_active: z.boolean(),
-	user_id: z.number(),
 	banned: z.boolean(),
 	suspended: z.boolean(),
-	wavier: z.date(),
-	created_at: z.date(),
-	modified_at: z.date(),
+	waiver: z.string(),
 	notes: z.string(),
-	activities: z.string(),
-	logins: z.date()
+	activities: z.array(z.string()),
+	logins: z.array(z.string()),
+	created_at: z.string(),
+	modified_at: z.string()
 });
 
 const testMember = {
 	id: 1001,
+	user_id: 'kungfu_panda',
 	phone: '3554255555',
 	email: 'frank@beans.com',
 	email_consent: true,
 	name: 'Frankfurter N Beans',
 	preferred_name: 'Frank',
 	date_of_birth: '1980-01-01',
-	guardian_name: '',
-	waiver: '2016-01-09 00:00:00+00',
+	guardian_name: 'H.D. Bunns',
 	is_active: true,
 	banned: false,
 	suspended: false,
-	created_at: '2017-05-18 04:27:09.503+00',
-	modified_at: '2018-02-07 02:36:31.766+00',
+	waiver: '2016-01-09 00:00:00+00',
 	notes: '',
 	activities: ['21cd9799b6', '3a5a719017', '0ebb0b5468', '84309225e7', 'c96d389517'],
-	logins: ['2018-02-07 02:36:31.766+00', '2018-02-11 02:53:24.334+00', '2018-02-27 04:21:18.658+00']
+	logins: [
+		'2018-02-07 02:36:31.766+00',
+		'2018-02-11 02:53:24.334+00',
+		'2018-02-27 04:21:18.658+00'
+	],
+	created_at: '2017-05-18 04:27:09.503+00',
+	modified_at: '2018-02-07 02:36:31.766+00'
 };
 
 export const getMember = async (id: number) => {
