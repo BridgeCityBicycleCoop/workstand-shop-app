cd3b74aad07e9f18a58ac31827ac17e863797c16
mitch <mitch@mukungfu.com>
2024-06-25 13:12:05 -0400

    update deploy notes/wiki

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
  import 'dotenv/config'
 console.log('PB TARGET', process.env.POCKETBASE_URL)
 console.log('ORIGIN', process.env.ORIGIN)
@@ -6,35 +7,44 @@ console.log('ORIGIN', process.env.ORIGIN)
  import helmet from "helmet";
  import http from 'http';
  import { createTerminus } from '@godaddy/terminus'
+=======
+import 'dotenv/config';
+console.log('PB TARGET', process.env.POCKETBASE_URL);
+import { handler } from './build/handler.js';
+import express from 'express';
+import helmet from 'helmet';
+import http from 'http';
+import { createTerminus } from '@godaddy/terminus';
+>>>>>>> Stashed changes
 
- const app = express();
+const app = express();
 
- app.use(
-   helmet({
-     contentSecurityPolicy: {
-       directives: {
-         ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-         "script-src": ["'self'", "'unsafe-inline'"],
-       }
-     },
-     referrerPolicy: {
-       policy: ["same-origin"],
-     },
-   })
- )
+app.use(
+	helmet({
+		contentSecurityPolicy: {
+			directives: {
+				...helmet.contentSecurityPolicy.getDefaultDirectives(),
+				'script-src': ["'self'", "'unsafe-inline'"]
+			}
+		},
+		referrerPolicy: {
+			policy: ['same-origin']
+		}
+	})
+);
 
- app.use(handler);
+app.use(handler);
 
- const server = http.createServer(app)
+const server = http.createServer(app);
 
- createTerminus(server, {
-   signals: ['SIGTERM', 'SIGINT'],
-   onSignal: async () => {
-     // Call your cleanup functions below. For example:
-     // db.shutdown()
-   }
- })
+createTerminus(server, {
+	signals: ['SIGTERM', 'SIGINT'],
+	onSignal: async () => {
+		// Call your cleanup functions below. For example:
+		// db.shutdown()
+	}
+});
 
- server.listen(3000, () => {
-   console.log('Listening on port 3000');
- });
+server.listen(3000, () => {
+	console.log('Listening on port 3000');
+});
