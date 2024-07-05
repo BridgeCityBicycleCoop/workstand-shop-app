56724575a620e785dcbb3a92b667e2d607ff5d9d
Adam Barrett <adam.barrett@thisdot.co>
2024-07-05 02:05:31 -0600

    fix top-nav colors

--- src/lib/ui/TopNav.svelte
+++ src/lib/ui/TopNav.svelte
@@ -73,7 +73,7 @@
 		top: 0;
 		left: calc(50% - var(--size));
 		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
+		border-top: var(--size) solid rgb(var(--color-primary));
 	}
 
 	nav a {
@@ -91,12 +91,12 @@
 	}
 
 	[aria-current='page'] > a {
-		color: var(--color-theme-1);
+		color: rgb(var(--color-primary));
 	}
 
 	@media (hover) {
 		a:hover {
-			color: var(--color-theme-1);
+			color: rgb(var(--color-primary));
 		}
 	}
 </style>
