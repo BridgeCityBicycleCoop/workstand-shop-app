a1d5fd46fa6aacc9831fe55e4ae85911d9080415
Mike 'mitch' Mitchel <mitch@nomadiklabs.com>
2024-07-08 10:28:01 -0400

    Bike list (#50)
    * update typegen and format code
    
    * bike list and single bike edit working
    
    ---------
    
    Co-authored-by: mitch <mitch@mukungfu.com>

--- src/routes/(shop)/bikes/+page.svelte
+++ src/routes/(shop)/bikes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
+	import { formatDistance } from 'date-fns';
 	import { goto } from '$app/navigation';
 	import { superForm } from 'sveltekit-superforms';
 	import BikeEditFields from '$lib/ui/forms/BikeEditFields.svelte';
+	import type { Bike } from '$lib/models/bike.js';
 
 	export let data;
+
+	const handleBikeUpdate = (_event: MouseEvent, bike: Bike) => {
+		goto(`/bikes/${bike.id}`);
+	};
 
 	const { form, errors, enhance, message } = superForm(data.form);
 </script>
@@ -45,6 +51,38 @@
 		>
 	</div>
 </div>
+
+<section class="bike-list">
+	<h3>Bike List</h3>
+	<div class="tableWrap">
+		{#if data.bikes.length > 0}
+			<table>
+				<thead>
+					<tr>
+						<th class="sticky-header">Serial Number</th>
+						<th class="sticky-header">Colour</th>
+						<th class="sticky-header">Make</th>
+					</tr>
+				</thead>
+				<tbody>
+					{#each data.bikes as bike}
+						<tr>
+							<td>
+								<button class="edit-profile" on:click={(event) => handleBikeUpdate(event, bike)}
+									>{bike.serialNumber}
+								</button>
+							</td>
+							<td>{bike.colour}</td>
+							<td>{bike.make}</td>
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		{:else}
+			<p>No bikes found</p>
+		{/if}
+	</div>
+</section>
 
 <style>
 	.bike-page {
@@ -99,14 +137,6 @@
 		box-sizing: border-box;
 	}
 
-	textarea {
-		padding: 12px 20px;
-		margin: 4px 0px;
-		border: 1px solid #ccc;
-		border-radius: 4px;
-		box-sizing: border-box;
-	}
-
 	.register-bike-buttons {
 		display: flex;
 		justify-content: center;
@@ -116,5 +146,9 @@
 		min-height: 40px;
 		margin: 25px 10px;
 		padding: 10px 30px;
+	}
+
+	.bike-list {
+		grid-area: activity;
 	}
 </style>
---
+++ src/routes/bikes/[id]/+page.server.ts
@@ -0,0 +1,59 @@
+import { fail, error } from '@sveltejs/kit';
+import { superValidate, message } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { bikeUpdateSchema } from '$lib/models/bike';
+import { bikes as bikesService } from '$lib/server/db';
+
+const bikeUpdateFormSchema = bikeUpdateSchema.pick({
+	id: true,
+	serialNumber: true,
+	make: true,
+	colour: true,
+	donatedBy: true,
+	email: true,
+	donationDate: true,
+	suggestedDonation: true,
+	cpicDate: true,
+	recipientName: true,
+	recipientAge: true,
+	recipientPhoneNumber: true,
+	outOfShopDate: true,
+	pricePaid: true,
+	bikeDestiny: true,
+	bcbcProgram: true,
+	notes: true
+});
+
+export async function load({ params }) {
+	const bike = await bikesService.get(params.id).catch((err) => {
+		if (err.status === 404) {
+			error(404, 'Not found');
+		}
+		throw err;
+	});
+
+	const form = await superValidate(bike, zod(bikeUpdateFormSchema));
+	return {
+		form: form,
+		bike: bike
+	};
+}
+
+export const actions = {
+	async default({ request }) {
+		const bikeUpdateForm = await superValidate(request, zod(bikeUpdateFormSchema));
+		if (!bikeUpdateForm.valid) return fail(400, { bikeUpdateForm });
+
+		try {
+			await bikesService.update(bikeUpdateForm.data);
+		} catch (error) {
+			if (error instanceof Error) {
+				return message(bikeUpdateForm, error.message, {
+					status: 400
+				});
+			}
+			throw error;
+		}
+		return { form: bikeUpdateForm };
+	}
+};
