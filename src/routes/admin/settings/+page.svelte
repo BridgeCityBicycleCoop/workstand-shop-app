2593ec08db28f1db308cc9d785dd9512e42b2cdd
mitch <mitch@nomadiklabs.com>
2024-07-22 12:26:41 -0400

    settings flex columns

--- src/routes/admin/settings/+page.svelte
+++ src/routes/admin/settings/+page.svelte
@@ -4,24 +4,57 @@
 </script>
 
 <h2>Shop Config</h2>
-<section>
+<section class="settings-list">
 	{#each Object.keys(shopConfig) as setting}
-		<tr>
-			<td>{camelCaseToWords(setting)}: </td>
-			<td>{shopConfig[setting]}</td>
-		</tr>
+		<div class="row">
+			<div class="column1">
+				<div><span class="setting-name">{camelCaseToWords(setting)}</span></div>
+			</div>
+			<div class="column2">
+				<div class="setting-value">{shopConfig[setting]}</div>
+			</div>
+		</div>
 	{/each}
 </section>
 
 <h2>Shop Theme</h2>
-<section>
+<section class="theme-list">
 	{#each Object.keys(theme) as style}
-		<tr>
-			<td>{camelCaseToWords(style)}: </td>
-			<td>{theme[style]}</td>
-		</tr>
+		<div class="row">
+			<div><span class="setting-name">{camelCaseToWords(style)}</span></div>
+			<div class="setting-value">{theme[style]}</div>
+		</div>
 	{/each}
 </section>
 
 <style>
+	.row {
+		display: flex;
+		width: 100%;
+		column-gap: 6rem;
+	}
+	.settings-list {
+		margin-top: 1rem;
+	}
+
+	.setting-name {
+		color: var(--color-accent-subtle);
+		background-color: var(--color-bg-med);
+		padding: 0px 10px;
+		text-align: left;
+		min-width: 175px;
+		flex-grow: 1;
+	}
+
+	.setting-name:after {
+		content: ':';
+	}
+
+	.setting-value {
+		flex-grow: 1;
+	}
+
+	h2 {
+		margin-top: 1em;
+	}
 </style>
