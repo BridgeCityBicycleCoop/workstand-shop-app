008fa82a5d061b19673ec1dfa26333b0f69034c5
Adam L Barrett <bigab@live.ca>
2024-07-30 07:24:27 -0600

    make bikes page a list and add registration page (#93)

--- src/routes/(shop)/bikes/+page.server.ts
+++ src/routes/(shop)/bikes/+page.server.ts
@@ -1,43 +1,7 @@
-import { fail } from '@sveltejs/kit';
-import { superValidate, message } from 'sveltekit-superforms';
-import { zod } from 'sveltekit-superforms/adapters';
-import { bikeCreateSchema } from '$lib/models';
 import { bikes as bikesService } from '$lib/server/db';
 
 export async function load() {
-	const form = await superValidate(zod(bikeCreateSchema));
-
-	const bikes = await bikesService.find();
-
 	return {
-		form: form,
-		bikes: bikes
+		bikes: await bikesService.find()
 	};
 }
-
-export const actions = {
-	async default({ request }) {
-		const form = await superValidate(request, zod(bikeCreateSchema));
-
-		if (!form.valid) {
-			return message(form, form.errors);
-		}
-		try {
-			await bikesService.add(form.data);
-			return message(form, 'Bike added successfully!');
-		} catch (errors) {
-			if (
-				typeof errors === 'object' &&
-				errors !== null &&
-				'request' in errors &&
-				typeof errors.request === 'object' &&
-				errors.request !== null &&
-				'status' in errors.request &&
-				typeof errors.request.status === 'number'
-			) {
-				return fail(errors.request.status ?? 500, { errors, form });
-			}
-			return fail(500, { errors, form });
-		}
-	}
-};
