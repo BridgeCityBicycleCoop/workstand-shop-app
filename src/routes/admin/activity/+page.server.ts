a7a091f00192d2d1acc6f2db1d0fdff7acf1bc80
Adam Barrett <bigab@live.ca>
2024-10-09 03:35:45 -0600

    feat: add hook that removes non-action URL query params with empty values

--- src/routes/admin/activity/+page.server.ts
+++ src/routes/admin/activity/+page.server.ts
@@ -1,9 +1,8 @@
-import { error, redirect } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import z from 'zod';
 import { message, superValidate, fail } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { toValidStartDate, toValidEndDate } from '$lib/server/utils/dates';
-import { hasEmptyUrlParams, clearEmptyUrlParams } from '$lib/utils';
 
 import { visits as visitsService, purposes as purposesService } from '$lib/server/db';
 
@@ -11,16 +10,12 @@ export const load = async ({ locals, url }) => {
 	if (!locals.user?.role?.includes('admin')) {
 		error(403, 'Not an admin');
 	}
-	if (hasEmptyUrlParams(url)) {
-		redirect(307, clearEmptyUrlParams(url).toString());
-	}
 
 	const startDate = url.searchParams.get('startDate') ?? '';
 	const endDate = url.searchParams.get('endDate') ?? '';
 	const startPage = url.searchParams.get('page') || '1';
 	const urlString = url.toString();
-	let page;
-	$: page = parseInt(startPage, 10);
+	const page = parseInt(startPage, 10);
 
 	const { visitsList, totalPages } = await visitsService.findByDate({
 		startDate: toValidStartDate(startDate),
